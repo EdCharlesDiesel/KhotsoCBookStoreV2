630b865f0217dcc764b4afbbcdc05d2f6623776e
Khotso Mokhethi <Mokhetkc@hotmail.com>
2021-07-22 10:14:35 +0200

    .

--- src/app/components/nav-bar/nav-bar.component.ts
+++ src/app/components/nav-bar/nav-bar.component.ts
@@ -3,6 +3,7 @@ import { Router } from '@angular/router';
 import { Observable, Subscription } from 'rxjs';
 import { User } from 'src/app/models/user';
 import { UserType } from 'src/app/models/UserType';
+import { AuthenticationService } from 'src/app/services/authentication.service';
 import { SubscriptionService } from 'src/app/services/subscription.service';
 import { UserService } from 'src/app/services/user.service';
 
@@ -14,14 +15,11 @@ import { UserService } from 'src/app/services/user.service';
 export class NavBarComponent implements OnInit, OnDestroy {
   userId;
   userDataSubscription: any;
-  userData= new User();
+  userData = new User();
   userType = UserType;
-  // wishListCount$: Observable<number>;
-  // cartItemCount$: Observable<number>;
-  // bookSubscriptionCount$: Observable<number>;
 
   constructor(
-    private userService: UserService,
+    private authService: AuthenticationService,
     private subscriptionService: SubscriptionService,
     private router: Router,) {
     this.userId = JSON.parse(localStorage.getItem('userId') || '{}');
@@ -31,8 +29,9 @@ export class NavBarComponent implements OnInit, OnDestroy {
   ngOnInit(): void {
     this.userDataSubscription = this.subscriptionService.userData.asObservable().subscribe(data => { this.userData = data; });
   }
-  logout() {
-    // this.authService.logout();
+
+  logout() : void{
+    this.authService.logout();
     this.router.navigate(['/login']);
   }
 
